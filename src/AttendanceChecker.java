f207c6db12167331e72400e282c1d984016d3507
AdityaSDivekar <adityadivekar1712@email.com>
2024-11-26 15:53:33 +0530

    Calculate Daily Wedge

--- src/AttendanceChecker.java
+++ src/AttendanceChecker.java
@@ -4,13 +4,29 @@ public class AttendanceChecker {
     public static void main(String[] args) {
         // Create a Random object
         Random random = new Random();
+        final int FULL_TIME_HOURS = 8;
+        final int PART_TIME_HOURS = 4;
+        final int WAGE_PER_HOUR = 20;
 
         // Generate a random number: 0 or 1
-        int randomValue = random.nextInt(2); // 0 or 1
+        int randomValue = random.nextInt(3); // 0 or 1
+        int workHours = 0;
+        String attendance;
+        if (randomValue == 0) {
+            attendance = "Absent";
+            workHours = 0;
+        } else if (randomValue == 1) {
+            attendance = "Part-Time";
+            workHours = PART_TIME_HOURS;
+        } else {
+            attendance = "Full-Time";
+            workHours = FULL_TIME_HOURS;
+        }
 
-        // Check attendance
-        String attendance = (randomValue == 1) ? "Present" : "Absent";
+        int dailyWage = workHours * WAGE_PER_HOUR;
 
-        // Print the result
+
         System.out.println("Employee is " + attendance);
-    }}
+        System.out.println("Daily Wage: $" + dailyWage);
+    }
+}
