8079fb809536cf06c79107b1e51d6e9c312cf231
AdityaSDivekar <adityadivekar1712@email.com>
2024-12-01 23:35:35 +0530

    Compute employee wedge for multiple companies

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -3,19 +3,18 @@ import java.util.Random;
 public class EmployeeWage {
     private static final int FULL_TIME_HOURS = 12;
     private static final int PART_TIME_HOURS = 8;
-    private static final int WAGE_PER_HOUR = 20;
-    private static final int MAX_WORKING_DAYS = 20;
-    private static final int MAX_WORKING_HOURS = 100;
 
-    private int totalWorkingHours = 0;
-    private int totalWorkingDays = 0;
-    private int totalMonthlyWage = 0;
 
-    public void computeEmployeeWage() {
+
+
+    public static void computeEmployeeWage(String companyName, int wagePerHour, int maxWorkingDays, int maxWorkingHours) {
+        int totalWorkingHours = 0;
+        int totalWorkingDays = 0;
+        int totalMonthlyWage = 0;
         Random random = new Random();
+        System.out.println("\nCalculating wage for " + companyName);
         System.out.println("Daily Wage Details:");
-
-        while (totalWorkingDays < MAX_WORKING_DAYS && totalWorkingHours < MAX_WORKING_HOURS) {
+        while (totalWorkingDays < maxWorkingDays && totalWorkingHours < maxWorkingHours) {
             int workHours = 0;
             String attendanceStatus;
 
@@ -36,35 +35,38 @@ public class EmployeeWage {
                     attendanceStatus = "Unknown";
             }
 
-            if (totalWorkingHours + workHours > MAX_WORKING_HOURS) {
-                workHours = MAX_WORKING_HOURS - totalWorkingHours;
+            if (totalWorkingHours + workHours > maxWorkingHours) {
+                workHours = maxWorkingHours - totalWorkingHours;
+            }
+
+            if (totalWorkingHours + workHours > maxWorkingHours) {
+                workHours = maxWorkingHours - totalWorkingHours;
             }
 
             totalWorkingHours += workHours;
             totalWorkingDays++;
-            int dailyWage = calculateDailyWage(workHours);
+            int dailyWage =  workHours * wagePerHour;
             totalMonthlyWage += dailyWage;
 
             System.out.println("Day " + totalWorkingDays + ": " + attendanceStatus + " - Daily Wage: $"
                     + dailyWage + " - Total Hours Worked: " + totalWorkingHours);
         }
 
-        printSummary();
+        printSummary(companyName, totalWorkingDays, totalWorkingHours, totalMonthlyWage);
     }
 
-    private int calculateDailyWage(int workHours) {
-        return workHours * WAGE_PER_HOUR;
-    }
-
-    private void printSummary() {
-        System.out.println("\nSummary:");
+    private static void printSummary(String companyName, int totalWorkingDays, int totalWorkingHours, int totalMonthlyWage) {
+        System.out.println("\nSummary for " + companyName + ":");
         System.out.println("Total Working Days: " + totalWorkingDays);
         System.out.println("Total Working Hours: " + totalWorkingHours);
         System.out.println("Total Monthly Wage: $" + totalMonthlyWage);
     }
 
+
+
     public static void main(String[] args) {
-        EmployeeWage employeeWage = new EmployeeWage();
-        employeeWage.computeEmployeeWage();
+        computeEmployeeWage("Company A",20,20,100);
+        computeEmployeeWage("Company B", 25, 22, 120);
+        computeEmployeeWage("Company C", 15, 18, 80);
     }
 }
